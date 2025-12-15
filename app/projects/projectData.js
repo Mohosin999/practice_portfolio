e95161bd1f2426bbde2ad3f4b661c47443eb14ab
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-12-15 07:52:48 +0600

    Remove a project from project section

--- app/projects/projectData.js
+++ app/projects/projectData.js
@@ -38,13 +38,13 @@ export const projectsData = [
     sourceUrl: "https://github.com/Mohosin999/Clean-YouTube-Application",
     caseStudyUrl: "/case-study/clean-youtube",
   },
-  {
-    image: "./img/live-cricket.png",
-    name: "Live Cricket Score",
-    description:
-      "Live Cricket Score App is a Next.js application powered by the Cricbuzz API, offering real-time cricket updates with a clean and responsive UI. It keeps users engaged with live scores, recent results, upcoming fixtures, and cricket articles. A detailed scorecard section provides in-depth match insights for a complete experience.",
-    liveUrl: "https://cricket-live-application-z7ba.vercel.app/",
-    sourceUrl: "https://github.com/Mohosin999/Live-Cricket-Score-App-Next.js",
-    caseStudyUrl: "/case-study/live-cricket",
-  },
+  // {
+  //   image: "./img/live-cricket.png",
+  //   name: "Live Cricket Score",
+  //   description:
+  //     "Live Cricket Score App is a Next.js application powered by the Cricbuzz API, offering real-time cricket updates with a clean and responsive UI. It keeps users engaged with live scores, recent results, upcoming fixtures, and cricket articles. A detailed scorecard section provides in-depth match insights for a complete experience.",
+  //   liveUrl: "https://cricket-live-application-z7ba.vercel.app/",
+  //   sourceUrl: "https://github.com/Mohosin999/Live-Cricket-Score-App-Next.js",
+  //   caseStudyUrl: "/case-study/live-cricket",
+  // },
 ];
