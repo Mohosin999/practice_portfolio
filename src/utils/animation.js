cc7bcdd2525a1a064f58a717689e4c759d53e313
Mohosin999 <mohosin.hasan.akash@gmail.com>
2026-04-13 08:05:27 +0600

    Add animation

--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,21 +1,21 @@
 export const fadeInUp = {
   initial: { opacity: 0, y: 100 },
   whileInView: { opacity: 1, y: 0 },
-  viewport: { once: true, amount: 0.3 },
-  transition: { duration: 0.5 },
+  viewport: { once: true, amount: 0.1 },
+  transition: { duration: 0.4 },
 };
 
 export const fadeInVisiable = {
   initial: { opacity: 0, scale: 0.8 },
   whileInView: { opacity: 1, scale: 1 },
-  viewport: { once: true, amount: 0.3 },
-  transition: { duration: 0.5 },
+  viewport: { once: true, amount: 0.1 },
+  transition: { duration: 0.4 },
 };
 
 export const staggerContainer = {
   initial: {},
   whileInView: { opacity: 1 },
-  viewport: { once: true, amount: 0.3 },
+  viewport: { once: true, amount: 0.1 },
   transition: { staggerChildren: 0.2 },
 };
 
