e86b20e57c24d05f73555edfe6a7baca767099fc
Mohosin999 <mohosin.hasan.akash@gmail.com>
2026-04-13 08:11:47 +0600

    add style

--- src/utils/animation.js
+++ src/utils/animation.js
@@ -6,7 +6,7 @@ export const fadeInUp = {
 };
 
 export const fadeInVisiable = {
-  initial: { opacity: 0, scale: 0.5 },
+  initial: { opacity: 0, scale: 0.2 },
   whileInView: { opacity: 1, scale: 1 },
   viewport: { once: true, amount: 0.1 },
   transition: { duration: 0.4 },
